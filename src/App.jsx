8b5b5fb71af6c90829f220e67df38db4d93211ee
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-07-23 18:46:17 -0300

    feat: creating li

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import "./App.css";
+import Banner from "./assets/images/bg-sidebar-mobile.svg";
 
 function App() {
   return (
     <>
-      <h1 className="text-5xl text-gray-600">Hello World!</h1>
+      <div className="border-2 w-[375px] ">
+        <div className="flex justify-center ">
+          <ul className="flex absolute z-10 gap-8 pt-10 ">
+            <li className="text-sm bg-amber-50 w-6 h-6 rounded-full pt-0.4">
+              1
+            </li>
+            <li className="text-sm text-amber-50 border-1 w-6 h-6 rounded-full">
+              2
+            </li>
+            <li className="text-sm text-amber-50 border-1 w-6 h-6 rounded-full">
+              3
+            </li>
+            <li className="text-sm text-amber-50 border-1 w-6 h-6 rounded-full">
+              4
+            </li>
+          </ul>
+          <img className="relative " src={Banner} alt="" />
+        </div>
+      </div>
     </>
   );
 }
