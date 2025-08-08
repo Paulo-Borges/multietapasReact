0ffc594b71aa43b1650497236d1b47694cb35c4a
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-08-08 18:14:58 -0300

    feat: Starting and fixes pickpage

--- src/pages/PickPage.jsx
+++ src/pages/PickPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../assets/images/bg-sidebar-mobile.svg";
+import Info from "../assets/images/bg-sidebar-desktop.svg";
 import checkmark from "../assets/images/icon-checkmark.svg";
 
 import { useNavigate } from "react-router-dom";
@@ -16,9 +17,9 @@ const PickPage = () => {
   // }
 
   return (
-    <div className=" w-[375px] h-[657px] bg-wite">
-      <div className=" w-[375px] h-[590px] relative bg-pink-50 ">
-        <div className="flex justify-center ">
+    <div className=" w-[375px] h-[657px] bg-white min-[1440px]:w-[1000px] min-[1440px]:h-[590px] min-[1440px]:p-2 min-[1440px]:rounded-md">
+      <div className=" w-[375px] h-[590px] relative bg-pink-50 min-[1440px]:bg-transparent">
+        <div className="flex justify-center min-[1440px]:hidden ">
           <ul className="flex absolute z-10 gap-8 pt-10 ">
             <li className="text-sm text-amber-50 border-1 w-6 h-6 rounded-full">
               1
@@ -35,9 +36,50 @@ const PickPage = () => {
           </ul>
           <img className="relative " src={Banner} alt="" />
         </div>
-        <form className="flex flex-col bg-white w-[335px] m-auto absolute left-5 top-26 p-6 items-start rounded-2xl">
+        <div className="max-[1439px]:hidden">
+          <div className="">
+            <ul className="flex flex-col absolute justify-end left-8 z-10 gap-8 pt-10 ">
+              <li className="text-sm text-amber-50 border-1 w-8 h-8 rounded-full p-2 flex gap-10 ">
+                1
+                <div className="flex flex-col text-start w-screen -mt-2">
+                  <span className="text-[10px] font-light text-gray-500">
+                    STEP 1
+                  </span>
+                  <h2 className="text-[12px] w-20 text-amber-50">YOUR INFO</h2>
+                </div>
+              </li>
+              <li className="text-sm text-amber-50 border-1 w-8 h-8 rounded-full p-2 flex gap-10 ">
+                2
+                <div className="text-start w-screen -mt-3">
+                  <span className="text-[10px] font-light text-gray-500">
+                    STEP 2
+                  </span>
+                  <h2 className="text-[12px] w-20 text-amber-50">
+                    SELECT PLAN
+                  </h2>
+                </div>
+              </li>
+              <li className="text-sm bg-amber-50 w-8 h-8 rounded-full p-2 flex gap-10">
+                3
+                <div className="text-start w-screen -mt-4">
+                  <span className="text-[10px] text-gray-500">STEP 3</span>
+                  <h2 className="text-[12px] w-20 text-amber-50">ADD-ONS</h2>
+                </div>
+              </li>
+              <li className="text-sm text-amber-50 border-1 w-8 h-8 rounded-full p-2 flex gap-10">
+                4
+                <div className="text-start w-screen -mt-4">
+                  <span className="text-[10px] text-gray-500">STEP 4</span>
+                  <h2 className="text-sm">SUMMARY</h2>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <img src={Info} alt="" />
+        </div>
+        <form className="flex flex-col bg-white w-[335px] min-[1440px]:w-[500px] m-auto absolute left-5 min-[1440px]:left-90 top-26 min-[1440px]:top-16 p-6 items-start rounded-2xl">
           <h1 className="font-bold text-2xl mb-3">Pick add-ons</h1>
-          <p className="flex text-left mb-3 text-neutral-400 ">
+          <p className="flex text-left mb-3 text-neutral-400 min-[1440px]:mb-10">
             Add-ons help enhance your gaming experience.
           </p>
 
@@ -77,7 +119,7 @@ const PickPage = () => {
               <p className="text-xs text-[#6032de]">+$2/mo</p>
             </div>
           </div>
-          <div className="flex items-center justify-around gap-4 p-4 border w-full border-gray-300 rounded  mb-4">
+          <div className="flex items-center justify-around gap-4 p-4 border w-full border-gray-300 rounded  mb-4 min-[1440px]:mb-16">
             <div className="w-5 h-5 p-1 bg-transparent border-1 border-gray-300 rounded-md"></div>
             <div className="flex flex-col items-start">
               <h2 className="text-xs font-semibold">Customizable profile</h2>
@@ -89,18 +131,33 @@ const PickPage = () => {
               <p className="text-xs text-[#6032de]">+$2/mo</p>
             </div>
           </div>
+          <div className="flex justify-between p-4 bg-white max-[1439px]:hidden min-[1440px]:flex min-[1440px]:w-full">
+            <button
+              className=" bg-transparent py-2 px-3 text-xs rounded text-neutral-400 cursor-pointer"
+              onClick={() => navigate("/select")}
+            >
+              Go Back
+            </button>
+
+            <button
+              className=" bg-blue-950 py-2 px-3 text-xs rounded text-white cursor-pointer"
+              onClick={handleNextStep}
+            >
+              Next Step
+            </button>
+          </div>
         </form>
       </div>
-      <div className="flex justify-between p-4 bg-white">
+      <div className="flex justify-between p-4 bg-white min-[1439px]:hidden">
         <button
-          className=" bg-transparent py-2 px-3 text-xs rounded text-neutral-400"
+          className=" bg-transparent py-2 px-3 text-xs rounded text-neutral-400 cursor-pointer"
           onClick={() => navigate(-1)}
         >
           Go Back
         </button>
 
         <button
-          className=" bg-blue-950 py-2 px-3 text-xs rounded text-white"
+          className=" bg-blue-950 py-2 px-3 text-xs rounded text-white cursor-pointer"
           onClick={handleNextStep}
         >
           Next Step
